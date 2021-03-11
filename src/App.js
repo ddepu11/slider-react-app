1326399c8c144183109f420360d4b0c7e577ae31
Deepanshu Tiwari <ddepu11@gmail.com>
2021-03-11 12:10:21 +0530

    Next , prev auto slide complete

--- src/App.js
+++ src/App.js
@@ -3,10 +3,41 @@ import {
   FaAngleDoubleLeft,
   FaQuoteRight,
 } from "react-icons/fa";
+import { useState, useEffect } from "react";
 import "./App.css";
 import data from "./data";
 
 function App() {
+  const [people, setPeople] = useState(data);
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    let slider = setInterval(() => {
+      if (index === people.length - 1) {
+        setIndex(0);
+      } else {
+        setIndex(index + 1);
+      }
+    }, 3000);
+    return () => clearInterval(slider);
+  }, [index]);
+
+  function nextSection() {
+    if (index === people.length - 1) {
+      setIndex(0);
+    } else {
+      setIndex(index + 1);
+    }
+  }
+
+  function prevSection() {
+    if (index === 0) {
+      setIndex(people.length - 1);
+    } else {
+      setIndex(index - 1);
+    }
+  }
+
   return (
     <div className="container">
       <header>
@@ -14,12 +45,23 @@ function App() {
       </header>
 
       <main className="hero flex">
-        <FaAngleDoubleLeft size="2em" className="left" />
+        <FaAngleDoubleLeft onClick={prevSection} size="2em" className="left" />
 
-        {data.map(({ image, name, title, quote }, index) => {
-          
+        {people.map(({ image, name, title, quote }, secIndex) => {
+          let position = "next";
+
+          if (index === secIndex) {
+            position = "center";
+          }
+          if (
+            index - 1 === secIndex ||
+            (index === 0 && secIndex === people.length - 1)
+          ) {
+            position = "prev";
+          }
+
           return (
-            <section key={index}>
+            <section key={secIndex} className={position}>
               <img src={image} alt="" />
               <h4>{name}</h4>
               <span>{title}</span>
@@ -29,7 +71,11 @@ function App() {
           );
         })}
 
-        <FaAngleDoubleRight size="2em" className="right" />
+        <FaAngleDoubleRight
+          onClick={nextSection}
+          size="2em"
+          className="right"
+        />
       </main>
     </div>
   );
