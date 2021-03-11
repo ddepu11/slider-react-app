96732e8b864c81c8653850a5ca8c156402f47600
Deepanshu Tiwari <ddepu11@gmail.com>
2021-03-11 10:38:57 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  FaAngleDoubleRight,
+  FaAngleDoubleLeft,
+  FaQuoteRight,
+} from "react-icons/fa";
+import "./App.css";
+import data from "./data";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container">
+      <header>
+        <h1>Reviews</h1>
       </header>
+
+      <main className="hero flex">
+        <FaAngleDoubleLeft size="2em" className="left" />
+
+        {data.map(({ image, name, title, quote }, index) => (
+          <section key={index} className={index === 2 ? "center" : ""}>
+            <img src={image} alt="" />
+            <h4>{name}</h4>
+            <span>{title}</span>
+            <p>{quote}</p>
+            <FaQuoteRight size="3em" className="quote" />
+          </section>
+        ))}
+
+        <FaAngleDoubleRight size="2em" className="right" />
+      </main>
     </div>
   );
 }
